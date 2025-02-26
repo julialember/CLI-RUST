3cc08f43b0129ea157d34620e7346db91a629755
Кот Шрёдингера <164202414+julialember@users.noreply.github.com>
2025-02-26 12:42:33 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-use std::{collections::HashMap, env::args, fs, io::Error};
+use std::{collections::HashMap, env::args, fs::{self}, io::Error};
 
 fn main() {
     let args = args().collect::<Vec<String>>();
-    let map: HashMap<String, Box<dyn Fn()>> = HashMap::from(
-        [
-            ("echo".to_string(),  Box::new(|| Echo::new(&args).task()) as Box<dyn Fn()>),
-            ("grep".to_string(), Box::new(|| Grep::new(&args).task())),
-            ("ls".to_string(), Box::new(|| Ls::new(&args).task()))
-        ]
-    );
-    if let Some(ts) = map.get(args[1].trim()){
-        ts();
-    } 
+    if args.len() > 1{
+        let map: HashMap<String, Box<dyn Fn()>> = HashMap::from(
+            [
+                ("echo".to_string(),  Box::new(|| Echo::new(&args).task()) as Box<dyn Fn()>),
+                ("grep".to_string(), Box::new(|| Grep::new(&args).task())),
+                ("ls".to_string(), Box::new(|| Ls::new(&args).task()))
+            ]
+        );
+        if let Some(ts) = map.get(args[1].trim()){
+            ts();
+        } else {println!("command: '{}' not found!", args[1])}
+    } else {println!("cant get arguments!")}
+    
 }
 
 trait Command<'a, C> where C: 'a{
@@ -26,21 +29,30 @@ impl<'a> Command<'a, Self> for Grep<'a>{
         Grep { args: &args }
     }
     fn task(&self) {
-        for i in open_file(self.args[2].trim())
-            .lines().filter(|line| line.contains(self.args[3].trim())){
-                println!("{}", i)
-            }
-    }
+        if self.args.len() < 4{
+            println!("not enought arguments!")
+        }else {
+        match open_file(self.args[2].trim()) {
+            Ok(text) => {
+                for i in text.lines().filter(|line| line.contains(self.args[3].trim())){
+                    println!("{}", i)
+                }}
+            Err(kind) => println!("error!: {}", kind),
+        }
+    }}
 }
 struct Ls<'a>{
     args: &'a Vec<String>
 }
 impl<'a> Command<'a, Self> for Ls<'a> {
     fn task(&self){
+        if self.args.len() < 3{
+            println!("not enought arguments!");
+        }else{
         match read_dir(&self.args[2]){
             Ok(()) => (),
             Err(kind) => println!("error!: {}", kind)
-        }
+        }}
     }
     fn new(args: &'a Vec<String>) -> Ls<'_> {
         Ls { args: &args }        
@@ -58,25 +70,32 @@ impl<'a> Command<'a, Self> for Echo<'a>{
         }
     }
     fn task(&self) {
+        if self.args.len() < 3{
+            println!("not enought elements!")
+        }else{
         for i in  &self.args[2..]{
             print!("{} ", i)
-        }
+        }}
     }
 }
 
-fn open_file(name: &str) -> String{
-    fs::read_to_string(name).unwrap()
+fn open_file(name: &str) -> Result<String, Error>{
+    fs::read_to_string(name)
 }
 
 fn read_dir(name: &String) -> Result<(), Error>{
     let dir =  fs::read_dir(name.trim())?;
     for i in dir{
-        let pat = i.unwrap().path();
-        println!("{}", pat.display());
-        if pat.is_dir(){
-            read_dir(&pat.display().to_string())?;
+        match i {
+            Ok(pat) => {
+                let pat = pat.path();
+                println!("{}", pat.display());
+                if pat.is_dir(){
+                read_dir(&pat.display().to_string())?;
+                drop(pat);
+                }}
+            Err(kind) => println!("error!:{}", kind),
         }
-        drop(pat);
     }
     Ok(())
 }
